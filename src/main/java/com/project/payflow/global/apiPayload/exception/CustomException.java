0b9df12e0cc0e337d2e2ed500adcc81b7bbdf636
Jimyeong Kim <kjm021221@icloud.com>
2026-06-14 17:10:13 +0900

    ✨feat: 예외처리 & 응답통일

---
+++ src/main/java/com/project/payflow/global/apiPayload/exception/CustomException.java
@@ -0,0 +1,14 @@
+package com.project.payflow.global.apiPayload.exception;
+
+import com.project.payflow.global.apiPayload.code.BaseErrorCode;
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException{
+
+    private final BaseErrorCode code;
+
+    public CustomException(BaseErrorCode errorCode) {
+        this.code = errorCode;
+    }
+}
