50a5405c25019f436ecc087af6e7d52d0aab3827
Jimyeong Kim <kjm021221@icloud.com>
2026-06-14 12:52:34 +0900

    first commit

---
+++ src/test/java/com/project/payflow/PayFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.payflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
